be1479afa4770b25f08e1b4d8895d6c59d7884b3
JJ <jj@MacBook-Pro-3.local>
2025-04-23 22:47:08 -0400

    Changed Command Title

--- slouch-detector/src/extension.ts
+++ slouch-detector/src/extension.ts
@@ -3,16 +3,9 @@ import * as path from 'path';
 
 // This method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "slouch-detector" is now active!');
-
-    const disposable = vscode.commands.registerCommand('slouch-detector.helloWorld', () => {
-        vscode.window.showInformationMessage('Hello World from Slouch Detector!');
-    });
-
-    context.subscriptions.push(disposable);
 
     context.subscriptions.push(
-        vscode.commands.registerCommand('slouch-detector.openCamera', async () => {
+        vscode.commands.registerCommand('PosturePolice.startApp', async () => {
             const chromeLauncher = await import('chrome-launcher'); // Dynamically import
             chromeLauncher.launch({
                 ignoreDefaultFlags: true,
